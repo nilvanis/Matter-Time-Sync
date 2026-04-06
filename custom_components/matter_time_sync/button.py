4553ace922e4efa3dc39ac5b2dd565261c40acf7
nilvanis <62594212+nilvanis@users.noreply.github.com>
2026-04-06 18:39:10 +0200

    make Matter Time Sync entity CONFIG

--- custom_components/matter_time_sync/button.py
+++ custom_components/matter_time_sync/button.py
@@ -22,6 +22,7 @@ from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -277,6 +278,7 @@ class MatterTimeSyncButton(ButtonEntity):
     """Button to sync time on a Matter device."""
 
     _attr_icon = "mdi:clock-check-outline"
+    _attr_entity_category = EntityCategory.CONFIG
     _attr_has_entity_name = True
     _attr_translation_key = "sync_time"
 
